26872e8f0d2ad16ef3838b9576035bf67791c699
bishoe01 <bishoe01@gmail.com>
2025-05-28 11:09:38 +0900

    init: 프로젝트 추가

---
+++ C3-4T2D/C3_4T2DApp.swift
@@ -0,0 +1,32 @@
+//
+//  C3_4T2DApp.swift
+//  C3-4T2D
+//
+//  Created by bishoe on 5/28/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct C3_4T2DApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
