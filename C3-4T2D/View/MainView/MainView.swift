43a6e497e7c7c0b0416e4db82e50eecd691f7de1
bishoe01 <bishoe01@gmail.com>
2025-05-29 17:47:37 +0900

    feat: main-view 진행중 섹션 수정

--- C3-4T2D/View/MainView/MainView.swift
+++ C3-4T2D/View/MainView/MainView.swift
@@ -2,44 +2,64 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Header()
-            // BANNER
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("미래적인 잠자리").font(.system(size: 17, weight: .bold))
-                        Text("2025.05.24 ~ 2025.07.02").font(.system(size: 11, weight: .regular))
+        ScrollView {
+            VStack(spacing: 0) {
+                Header()
+
+                // BANNER
+                VStack {
+                    HStack { // 진행중 프로젝트 섹션
+                        VStack(alignment: .leading) {
+                            Text("미래적인 잠자리").font(.system(size: 17, weight: .bold))
+                            Text("2025.05.24 ~ 2025.07.02").font(.system(size: 11, weight: .regular))
+                        }
+                        Spacer()
+                        Image(systemName: "ellipsis").foregroundColor(.gray).font(.system(size: 24))
+                    }.padding(.vertical, 12)
+
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 10) {
+                            ImageCard()
+                            ImageCard()
+                            ImageCard()
+                        }
                     }
-                    Spacer()
-                    Image(systemName: "ellipsis").foregroundColor(.gray).font(.system(size: 24))
-                }.padding(.vertical, 12)
+                }.padding(.horizontal, 20)
+                    .padding(.bottom, 30)
+
+                Divider()
+                    .overlay(
+                        Rectangle()
+                            .fill(Color.gray)
+                            .frame(height: 12)
+                    )
+
+                // 전체 프로젝트 섹션
+                VStack {
+                    HStack {
+                        HStack {
+                            Text("전체보기").font(.system(size: 17, weight: .semibold))
+                            Image(systemName: "chevron.down")
+                        }
+                        Spacer()
+                        HStack(spacing: 16) {
+                            Image(systemName: "rectangle.grid.2x2.fill")
+                            Image(systemName: "rectangle.grid.2x2.fill")
+                            Image(systemName: "rectangle.grid.1x2.fill")
+                        }
+                    }.padding(.horizontal, 20)
+                        .padding(.bottom, 16)
+
+                    LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 10), count: 3), spacing: 10) {
+                        // MARK: 나중에 배열의 길이에 따라 반복문이 돌아갈 예정  지금은 여러개 보이게 15개로 고정
+
+                        ForEach(0 ..< 15, id: \.self) { index in // 더 많은 아이템으로 변경
+                            GridImageCard(index: index)
+                        }
+                    }
                     .padding(.horizontal, 20)
-
-                ZStack(alignment: .bottom) {
-                    // Rectangle -> Image로 바뀔예정
-                    Rectangle()
-                        .fill(
-                            Color.gray
-                        )
-                        .frame(height: 280)
-
-                    VStack(alignment: .center, spacing: 4) {
-                        Text("그림그리는 게 참 어렵다. 아이디어 구상에 시간을 너무 많이...")
-                            .foregroundColor(.white)
-                            .font(.system(size: 11, weight: .semibold))
-                            .lineLimit(1)
-
-                        Text("05.22 13:23pm")
-                            .foregroundColor(.white.opacity(0.8))
-                            .font(.system(size: 11, weight: .regular))
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(.horizontal, 8) // Horizontal padding몇 ?
-                    .padding(.vertical, 10)
-//                    #4B4B4B opacity0.4
-                    .background(Color(red: 75 / 255, green: 75 / 255, blue: 75 / 255).opacity(0.4))
                 }
+                .padding(.vertical, 20)
             }
         }
     }
@@ -48,4 +68,3 @@ struct MainView: View {
 #Preview {
     MainView()
 }
-
