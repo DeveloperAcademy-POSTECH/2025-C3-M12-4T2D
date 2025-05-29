50aa8bd711c016b892069bf4f86be3a724e307fe
hiseyeon <se020122@naver.com>
2025-05-29 11:37:27 +0900

    feat: Camera 폴더 생성

---
+++ C3-4T2D/View/Camera/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  C3-4T2D
+//
+//  Created by Hwnag Seyeon on 5/29/25.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
