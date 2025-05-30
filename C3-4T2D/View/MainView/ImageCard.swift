5d8f61bca4a7bfd476c9c074276af7d2d6fd1bbc
bishoe01 <bishoe01@gmail.com>
2025-05-30 17:34:13 +0900

    feat: main-view bannerImage rectangle -> image에 맞게 수정

--- C3-4T2D/View/MainView/ImageCard.swift
+++ C3-4T2D/View/MainView/ImageCard.swift
@@ -1,7 +1,13 @@
 import SwiftUI
 
 struct ImageCard: View {
+    let image: String
     var body: some View {
-        Rectangle().foregroundColor(.gray).frame(width: 193, height: 137).cornerRadius(5)
+        Image("tmpImage")
+            .resizable()
+            .frame(width: 193, height: 137)
+            .cornerRadius(5)
+
+//        Rectangle().foregroundColor(.gray).frame(width: 193, height: 137).cornerRadius(5)
     }
 }
