710428fd0fc03311a89113e88143df342d6f0313
bishoe01 <bishoe01@gmail.com>
2025-05-31 20:50:59 +0900

    feat: main-view Header 디자인 수정사항 반영 - 병렬화

--- C3-4T2D/View/MainView/Header.swift
+++ C3-4T2D/View/MainView/Header.swift
@@ -2,27 +2,29 @@ import SwiftUI
 
 struct Header: View {
     var body: some View {
-        HStack(spacing: 22) {
+        HStack(spacing: 30) {
             Image(systemName: "person.circle")
                 .resizable()
-                .frame(width: 35, height: 35)
+                .frame(width: 49, height: 49)
                 .tint(Color.yellow)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("국민대학교 합격")
+                        .font(.system(size: 12, weight: .semibold))
+                    Text("D-129")
+                        .font(.system(size: 11))
+                        .padding(.horizontal, 4)
+                        .padding(.vertical, 1.5)
+                        .background(Color.orange.opacity(0.2))
+                }
 
-            HStack {
-                Text("D-129")
-                    .font(.system(size: 11))
-                    .padding(.horizontal, 4)
-                    .padding(.vertical, 1.5)
-                    .background(Color.orange.opacity(0.2))
-                Text("국민대학교 합격")
-                    .font(.system(size: 12, weight: .semibold))
+                HStack(spacing: 11) {
+                    Text("🔥489").font(.system(size: 15, weight: .semibold))
+                    Text("📒12").font(.system(size: 15, weight: .semibold))
+                    Text("🎨112").font(.system(size: 15, weight: .semibold))
+                }
             }
-
-            HStack(spacing: 11) {
-                Text("🔥489").font(.system(size: 15, weight: .semibold))
-                Text("📒12").font(.system(size: 15, weight: .semibold))
-                Text("🎨112").font(.system(size: 15, weight: .semibold))
-            }
+            Spacer()
         }.padding(.vertical, 24)
             .padding(.horizontal, 20)
     }
