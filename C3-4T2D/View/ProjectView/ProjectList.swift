d9997e01a797bbd9658143475f1686a87d6e7a18
bishoe01 <bishoe01@gmail.com>
2025-06-05 00:56:54 +0900

    feat: ProjectList swiftdata 연결  resolve #38

--- C3-4T2D/View/ProjectView/ProjectList.swift
+++ C3-4T2D/View/ProjectView/ProjectList.swift
@@ -5,20 +5,30 @@
 //  Created by 차원준 on 5/29/25.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ProjectList: View {
+    let project: Project
+    @Query private var posts: [Post]
+
+    init(_ project: Project) {
+        self.project = project
+        // 입력받은 프로젝트 기반 포스트
+        self._posts = Query(SwiftDataManager.getPostsForProject(project))
+    }
+
     var body: some View {
         ScrollView {
             LazyVStack(spacing: 25) {
-                ForEach(0 ..< 20) { _ in
-                    ProjectView()
+                ForEach(posts, id: \.id) { post in
+                    PostView(post: post)
                 }
             }
-        }
+        }.navigationTitle(project.projectTitle) // Header에 프로젝트 title 노출
     }
 }
 
-#Preview {
-    ProjectList()
-}
+// #Preview {
+//    MainView()
+// }
