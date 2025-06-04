f19b66b46d43539924703e518d5d16e27284a467
더연 <r0mancesi1z0@gmail.com>
2025-06-04 15:50:18 +0900

    feat: onboarding-view UI구현 및 유효성 판단 로직 추가 (#33)
    * feat: onboarding-view UI구현 및 유효성 판단 로직 추가
    
    * Feat: OnboardingView 충돌 및 오타 수정
    
    * feat: onboarding-view hidingkeyboard 중복 수정

--- C3-4T2D/App/C3_4T2DApp.swift
+++ C3-4T2D/App/C3_4T2DApp.swift
@@ -27,7 +27,8 @@ struct C3_4T2DApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            OnboardingView()
         }
         .modelContainer(sharedModelContainer)
     }
