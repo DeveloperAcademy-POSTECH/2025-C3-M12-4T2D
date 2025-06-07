728b9a1991eb2108f05baf277364a7cb6afaabab
bishoe01 <bishoe01@gmail.com>
2025-06-07 16:30:41 +0900

    feat: splash-view-version2 UI 및 데이터연동

---
+++ C3-4T2D/View/SplashView/SplashView2.swift
@@ -0,0 +1,104 @@
+//
+//  SplashView2.swift
+//  C3-4T2D
+//
+//  Created by bishoe on 6/7/25.
+//
+
+import SwiftData
+import SwiftUI
+
+struct SplashView2: View {
+//    @Environment(Router.self) var router
+
+    @Query private var users: [User]
+    var currentUser: User { users.first! }
+
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            LinearGradient(
+                gradient: Gradient(stops: [
+                    .init(color: .white.opacity(0.3), location: 0.0),
+                    .init(color: .white.opacity(0.3), location: 0.3806),
+                    .init(color: .prime1.opacity(0.3), location: 0.9351),
+                    .init(color: .prime1.opacity(0.3), location: 1.0)
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+
+            VStack {
+                VStack(alignment: .leading, spacing: 30) {
+                    VStack(alignment: .leading, spacing: 4) {
+                        HStack(spacing: 8) {
+                            Text(currentUser.nickname)
+                                .font(.title)
+                                .bold()
+                                .foregroundColor(.prime1)
+                            Text("님")
+                                .font(.title)
+                                .bold()
+                        }
+
+                        Text("망고에 오신 걸 환영합니다!")
+                            .font(.title)
+                            .bold()
+                    }
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("\(currentUser.remainingDays)까지")
+                            .font(.system(size: 17, weight: .semibold))
+                            .foregroundColor(.gray3)
+                        Text("\(currentUser.userGoal)")
+                            .font(.system(size: 17, weight: .semibold))
+                            .foregroundColor(.gray3)
+                        Text("목표까지 망고가 함께할게요")
+                            .font(.system(size: 17, weight: .regular))
+                            .foregroundColor(.gray3)
+                    }
+                }
+                .padding(.horizontal, 30)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 50)
+
+                Spacer()
+            }
+            .zIndex(1)
+
+            Image("splash2")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(
+                    width: UIScreen.main.bounds.width,
+                    height: UIScreen.main.bounds.height * 0.74
+                )
+                .position(
+                    x: UIScreen.main.bounds.width * 1.1/2,
+                    y: UIScreen.main.bounds.height - (UIScreen.main.bounds.height * 0.7) * 1.7/4
+                )
+                .ignoresSafeArea(edges: .bottom)
+                .zIndex(0)
+
+            VStack {
+                Spacer()
+                Button(action: {}) {
+                    Text("시작하기")
+                        .font(.headline)
+                        .foregroundColor(.orange)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(.white)
+                        .cornerRadius(15)
+                }
+                .padding(.horizontal, 16)
+                .padding(.bottom, 32)
+            }
+            .zIndex(2)
+        }
+    }
+}
+
+#Preview {
+    SplashView2().modelContainer(for: [User.self, Project.self, Post.self], inMemory: true)
+        .environment(Router())
+}
